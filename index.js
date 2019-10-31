fe8c1a2df94080dd4f1cfc8873860485bcf18c88
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-10-31 17:54:14 +0100

    connected MongoDB to Apollo + added basic query & mutation for dog

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+require('dotenv').config()
 const { ApolloServer, gql } = require('apollo-server')
+const mongoose = require('mongoose')
+
+const Dog = require('./models/dog')
 
 const typeDefs = gql`
-  type Query {
-    hello: String!
+  type Dog {
+    id: ID!
+    name: String!
+    age: Int!
   }
+  type Mutation {
+    addDog(name: String!, age: Int!): Dog!
+  }
+
+  type Query {
+    getDogs: [Dog]
+  }
+
 `
 
 const resolvers = {
   Query: {
-    hello: () => 'hello world!'
+    getDogs: async () => {
+      try {
+        const dogs = await Dog.find()
+        return dogs
+      } catch (err) {
+        throw new Error(err)
+      }
+    }
+  },
+  Mutation: {
+    addDog: async (_, {name, age}) => {
+      const newDog = new Dog({
+        name,
+        age
+      })
+
+      const res = await newDog.save()
+      return newDog
+    }
   }
 }
 
@@ -16,5 +48,7 @@ const server = new ApolloServer({ typeDefs, resolvers })
 
 const port = 4000
 
-server.listen({ port })
+mongoose
+  .connect(process.env.MONGODB, { useNewUrlParser: true })
+  .then(() => server.listen({ port }))
   .then(() => console.log(`server running on port ${port}`))
